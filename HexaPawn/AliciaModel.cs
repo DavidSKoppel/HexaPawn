406fd81e0211cebed7e73a49e689869abd2dc1fb
Minhly <minh.y.ly@hotmail.com>
2023-08-24 09:07:14 +0200

    Sexy model added

---
+++ HexaPawn/AliciaModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexaPawn
+{
+    public class AliciaModel
+    {
+        public string MoveFrom { get; set; }
+        
+        public string MoveTo { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
